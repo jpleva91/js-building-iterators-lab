15d766938d1afa1009c13b40520d67323553a685
jpleva91 <jpleva91@gmail.com>
2017-09-18 17:27:09 -0600

    changes challenge 1 and 2

--- myReduce.js
+++ myReduce.js
@@ -2,9 +2,11 @@
 // Don't worry about initialValue at first. You can always add it in later.
 
 function myReduce(arr, callback) {
-
-//  CODE INSIDE HERE   //
-
+	var newArr = [];
+	for (let i = 0; i < arr.length; i++) {
+		newArr.push(callback(arr[i], i, arr));
+	}
+	return newArr;
 }
 
 /*
